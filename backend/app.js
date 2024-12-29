cc3c9091c4813fced8676db4356d546eb745caaf
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-12-29 16:47:24 -0700

    feat: add API routes
    Add GET API requests

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.get('/api/pets', async (req, res) => {
+  const pets = await prisma.pets.findMany();
+  return res.json(pets);
+});
+
+app.get('/api/pets/:category', async (req, res) => {
+  let category = req.params.category.toLocaleLowerCase();
+  // Capitalize first letter
+  category = category[0].toLocaleUpperCase() + category.slice(1);
+
+  const pets = await prisma.pets.findMany({
+    where: {category: category},
+  });
+  return res.json(pets);
+});
+
+app.listen(process.env.PORT, () => console.log(`Listening on PORT ${process.env.PORT}`));
