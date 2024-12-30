7d1a4593993f77f3d1caada0041e4941a8212234
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-12-29 23:49:28 -0700

    refactor: add routing + reorganize folder structure
    Add path aliasing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@styles/index.css'
+import routes from './routes'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
