c68ddb54ec32c6ef3fbbc144b931db2a5c669a8e
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2025-01-05 15:23:53 -0700

    refactor: css styles
    Add var names for colors, make font sizes more responsive, move global
    styling to index.css

--- frontend/src/components/CategoryCard.tsx
+++ frontend/src/components/CategoryCard.tsx
@@ -12,7 +12,7 @@ const CategoryCard:FC<CardProps> = (props) => {
   return (
     <div className="category-card">
       <img src={props.img} alt={props.category} />
-      <div className="text">
+      <div className="category-card-text">
         <h3>{props.category}</h3>
         <p>{props.desc}</p>
       </div>
